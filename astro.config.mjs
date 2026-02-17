ca8a877d2bf8acce93d068788083ac1fa16698c6
Guilherme <gui.mury.gm@gmail.com>
2026-02-17 20:56:00 -0300

    fix: remove deprecated hybrid output option for astro v5 and content changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://kilserv.vercel.app/',
-  output: 'hybrid', // <--- IMPORTANT
   adapter: vercel(),
   compressHTML: true
 });
