c51027ed74651d3281dc3f38a63c6f79242509b8
Guilherme <gui.mury.gm@gmail.com>
2026-02-17 21:15:48 -0300

    fix: disable checkOrigin to resolve 403 forbidden on api

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://kilserv.vercel.app/',
-  output: 'static', // Back to static
+  site: 'https://kilserv.vercel.app',
+  output: 'static',
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
+  
+  security: {
+    checkOrigin: false
+  },
 });
